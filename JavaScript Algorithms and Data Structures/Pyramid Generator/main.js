398571b0427b8e21a138c78105573f625e3945e4
urega <regarevautama@gmail.com>
2025-02-04 10:26:40 +0700

    modify pyramid generator

--- JavaScript Algorithms and Data Structures/Pyramid Generator/main.js
+++ JavaScript Algorithms and Data Structures/Pyramid Generator/main.js
@@ -1,1 +1,36 @@
-console.log(test);
+// Pyramid generator
+
+const character = "!";
+const count = 8;
+const rows = [];
+let result = "";
+let inverted = false;
+
+//for loop
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+// while loop
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+//for of
+for (const row of rows) {
+  result = result + row + "\n";
+}
+//function
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+console.log(result);
